8934e4187c7043d02f988486ef937eb2f5679440
3ele-projects <github@3ele.de>
2019-09-08 20:57:24 +0200

    master

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ import svgwrite
 import whratio
 import lxml
 from lxml import etree # Ubuntu Karmic package: python-lxml
-from xml.dom import minidom
+from cairosvg import svg2png
+
 import svgutils.transform as sg
 
 
 
 
 def create_svg_templates (width, height, name):
-    dwg = svgwrite.Drawing(name+'.svg', profile='tiny', size =(width, height))
+    dwg = svgwrite.Drawing(name+'.svg', profile='tiny', size =(width, height), )
+    dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(255,255,255)'))
     dwg.save()
     return name+'.svg'
 
@@ -31,7 +33,7 @@ def check_oriantation(width, height):
         return False
 
 
-def parse_svg(fname):
+def create_svg_image(fname):
     tree = etree.parse(fname)
     root = tree.getroot()
     margin = 10
@@ -45,26 +47,19 @@ def parse_svg(fname):
     element_width = int(logo.attrib['width'])
 
 
-    center_element_x = template_width / 2- (element_width / 2)
-    center_element_y = template_height / 2 - (element_height / 2)
 
-
-    logo.attrib['x'] =str (center_element_x)
-    logo.attrib['y'] = str(center_element_y)
-    logo.attrib['width'] =str (element_width)
-    logo.attrib['height'] = str(element_height)
 
 
     root.append(logo)
-    print('Template width: '+ root.attrib['width'])
-    print('Template height: '+ root.attrib['height'])
-    print('Logo width: ' +logo.attrib['width'])
-    print ('Logo height: '+logo.attrib['height'])
-
-    print('Logo Position horizontal: ' + logo.attrib['x'])
-    print ('Logo Position vertical: '+logo.attrib['y'])
 
 
+
+    if check_oriantation(logo.attrib['width'], logo.attrib['height']):
+        print('Logo is horizontal')
+    element_width = template_width- margin
+    logo.attrib['width'] = str(element_width)
+    element_height = template_height- margin
+    logo.attrib['height'] = str(element_height)
     if element_width > template_width:
         element_width = template_width - margin
         logo.attrib['x'] = str(margin)
@@ -77,6 +72,21 @@ def parse_svg(fname):
         logo.attrib['y'] = str(margin)
         logo.attrib['height'] = str(element_height)
 
+    center_element_x = template_width / 2 - (element_width / 2)
+    center_element_y = template_height / 2 - (element_height / 2)
+
+    logo.attrib['x'] = str(center_element_x)
+    logo.attrib['y'] = str(center_element_y)
+    logo.attrib['width'] = str(element_width)
+    logo.attrib['height'] = str(element_height)
+
+    print('Template width: '+ root.attrib['width'])
+    print('Template height: '+ root.attrib['height'])
+    print('Logo width: ' +logo.attrib['width'])
+    print ('Logo height: '+logo.attrib['height'])
+
+    print('Logo Position horizontal: ' + logo.attrib['x'])
+    print ('Logo Position vertical: '+logo.attrib['y'])
     doc = etree.ElementTree(root)
     doc.write(fname)
 
@@ -90,9 +100,9 @@ def main():
         template_dic = csv.DictReader(csvfile)
         for template in template_dic:
             file_name = create_svg_templates(template['width'], template['height'], template['name'] )
-
-
-            logo = parse_svg(file_name)
+            logo = create_svg_image(file_name)
+            svg2png(
+                url=template['name']+'.svg', write_to=template['name']+'.png')
 
 
 
